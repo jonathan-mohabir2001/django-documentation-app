874ecbdaf31e8c0e82f9a492bc677975dbccb1a7
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-03-21 15:08:37 -0400

    activiated models and posted them to the django admin, created super users

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here
+
+from .models import Question 
+
+#THIS IS HOW MODELS ARE REGISTERED
+admin.site.register(Question)
