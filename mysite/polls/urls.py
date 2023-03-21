3f0e2b809b1512219dfe87f2733641ded3857c7b
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-03-21 11:31:48 -0400

    URL dipat cher properly working

---
+++ mysite/polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+
+from . import views
+# from this directory import the views.py and all of its functions
+
+# Access the index function of the views file
+urlpatterns = [
+    path('', views.index, name='index'),
+]
